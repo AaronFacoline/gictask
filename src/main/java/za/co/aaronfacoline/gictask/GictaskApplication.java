e176de6d2cd6b94f2af7dbf004b355daf35cc68e
AaronFacoline <apfacoline@tutanota.com>
2025-04-05 07:37:48 +0200

    Initial commit

---
+++ src/main/java/za/co/aaronfacoline/gictask/GictaskApplication.java
@@ -0,0 +1,13 @@
+package za.co.aaronfacoline.gictask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GictaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GictaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/aaronfacoline/gictask/GictaskApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.aaronfacoline.gictask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GictaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
